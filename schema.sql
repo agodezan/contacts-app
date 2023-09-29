207a985ca56c43c6543144d3fec0cac0452c006b
vplechuc <vplechuc@gmail.com>
2023-09-28 23:34:26 -0300

    refactoring files

--- schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS persons;
-
-CREATE TABLE persons (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL ,
-    emailaddress TEXT NOT NULL,
-    whatsapp INTEGER NOT NULL
-);
